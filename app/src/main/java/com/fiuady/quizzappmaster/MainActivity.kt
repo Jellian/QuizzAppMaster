273c0a508af245d6ef0d58aedbd36a7ce17dd235
Jellian <haanniiaarodriguez@gmail.com>
2020-11-14 22:05:48 -0600

    Activity opciones & viewmodel añadido
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizzappmaster/MainActivity.kt
+++ app/src/main/java/com/fiuady/quizzappmaster/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.fiuady.quizzappmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
 
+private const val CHEAT_ACTIVITY_REQUEST_CODE=0;
 class MainActivity : AppCompatActivity() {
+    private lateinit var jugar_button : Button
+    private lateinit var opciones_button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gameModel:GameModel by viewModels()
+
+        jugar_button=findViewById(R.id.jugar_button)
+        opciones_button= findViewById(R.id.opciones_button)
+
+        jugar_button.setOnClickListener{_->
+            val intent = Intent(this@MainActivity, Juego::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        opciones_button.setOnClickListener{_->
+            val intent = Intent(this@MainActivity, Opciones::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
