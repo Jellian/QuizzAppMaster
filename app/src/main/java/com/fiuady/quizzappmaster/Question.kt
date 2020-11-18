899ea46860312988aed4471ecf342e4fe167c6a3
Jellian <haanniiaarodriguez@gmail.com>
2020-11-17 18:30:54 -0600

    creacion final de la clase Question & creación de Array con preguntas
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizzappmaster/Question.kt
+++ app/src/main/java/com/fiuady/quizzappmaster/Question.kt
@@ -1,5 +1,10 @@
 package com.fiuady.quizzappmaster
 
-data class Question (val topic: Int, val resId: Int, val answer:Boolean, var answered:Boolean, var correct:Boolean) {
+data class Question(val question_number: Int, val questionText: String, val topic: Int, val answer1: String, val answer2: String,
+                    val answer3: String, val answer4: String,val answer: String, var enabled: Boolean,  var cheated: Boolean,
+                    val answered: Boolean, var correct: Boolean, val resp1:Boolean,val resp2:Boolean,val resp3:Boolean,val resp4:Boolean) {
+
+
+
 
 }
