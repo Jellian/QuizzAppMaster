d525d31de6ae1e9895d04d6c92bba44fd6c3c8d6
Jellian <haanniiaarodriguez@gmail.com>
2020-11-20 10:23:56 -0600

    actualizacion de error }<-----
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizzappmaster/Opciones.kt
+++ app/src/main/java/com/fiuady/quizzappmaster/Opciones.kt
@@ -67,183 +67,185 @@ class Opciones : AppCompatActivity() {
             musica_checkbox.isChecked = true
             arte_checkbox.isChecked = true
             videojuegos_checkbox.isChecked = true
-          
-        if (!switch.isChecked()) {
-            spinnerpistas.setEnabled(false)
         }
 
-
-        if (!cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && videojuegos_checkbox.isChecked || cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked ||
-                !cine_checkbox.isChecked && ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked || !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked ||
-                !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked || !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && musica_checkbox.isChecked && !videojuegos_checkbox.isChecked) {
-            spinner.isEnabled = false
-            spinner.setSelection(0)
-        } else {
-            spinner.isEnabled = true
-        }
-
-        cine_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                cont += 1
-                if (!topicsarray.contains(1)) topicsarray.add(1)
-
-            } else if (cont == 1) {
-                cine_checkbox.isChecked = true
-
-            } else {
-                cont -= 1
-                topicsarray.remove(1)
-
+            if (!switch.isChecked()) {
+                spinnerpistas.setEnabled(false)
             }
-            validar()
-        }
-
-        ciencia_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                cont += 1
-                if (!topicsarray.contains(3)) topicsarray.add(3)
-            } else if (cont == 1) {
-                ciencia_checkbox.isChecked = true
-            } else {
-                cont -= 1
-                topicsarray.remove(3)
-            }
-            validar()
-        }
-        deporte_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                cont += 1
-                if (!topicsarray.contains(4)) topicsarray.add(4)
-            } else if (cont == 1) {
-                deporte_checkbox.isChecked = true
-            } else {
-                cont -= 1
-                topicsarray.remove(4)
-            }
-            validar()
-        }
-        arte_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                cont += 1
-                if (!topicsarray.contains(5)) topicsarray.add(5)
-            } else if (cont == 1) {
-                arte_checkbox.isChecked = true
-            } else {
-                cont -= 1
-                topicsarray.remove(5)
-            }
-            validar()
-        }
-        musica_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                cont += 1
-                if (!topicsarray.contains(2)) topicsarray.add(2)
-            } else if (cont == 1) {
-                musica_checkbox.isChecked = true
-            } else {
-                cont -= 1
-                topicsarray.remove(2)
-            }
-            validar()
-        }
-        videojuegos_checkbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                prueba.text = topicsarray[0].toString()
-                cont += 1
-                if (!topicsarray.contains(6)) topicsarray.add(6)
-            } else if (cont == 1) {
-                videojuegos_checkbox.isChecked = true
-            } else {
-                cont -= 1
-                topicsarray.remove(6)
-            }
-            validar()
-        }
 
 
-        todoschek.setOnClickListener {
-            if (todoschek.isChecked == true) {
-                todoschek.isEnabled = false
-                ciencia_checkbox.isChecked = true
-                cine_checkbox.isChecked = true
-                deporte_checkbox.isChecked = true
-                musica_checkbox.isChecked = true
-                arte_checkbox.isChecked = true
-                videojuegos_checkbox.isChecked = true
-                spinner.setSelection(1)
-            }
-        }
-
-        switch.setOnCheckedChangeListener { _, _ ->
-            if (!switch.isChecked) {
-                spinnerpistas.isEnabled = false
-            }
-            if (switch.isChecked) {
-                spinnerpistas.isEnabled = true
-            }
-        }
-    }
-
-    fun validar() {
-        when (cont) {
-            1 -> {
-                spinadp()
-                todoschek.isEnabled = true
-                todoschek.isChecked = false
-                spinner.setSelection(0)
+            if (!cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && videojuegos_checkbox.isChecked || cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked ||
+                    !cine_checkbox.isChecked && ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked || !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && deporte_checkbox.isChecked && !arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked ||
+                    !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && arte_checkbox.isChecked && !musica_checkbox.isChecked && !videojuegos_checkbox.isChecked || !cine_checkbox.isChecked && !ciencia_checkbox.isChecked && !deporte_checkbox.isChecked && !arte_checkbox.isChecked && musica_checkbox.isChecked && !videojuegos_checkbox.isChecked) {
                 spinner.isEnabled = false
-            }
-            in 2..5 -> {
-                spinadp()
-                todoschek.isEnabled = true
-                todoschek.isChecked = false
                 spinner.setSelection(0)
+            } else {
                 spinner.isEnabled = true
             }
-            6 -> {
-                spinadp()
-                todoschek.isChecked = true
-                todoschek.isEnabled = false
-                spinner.setSelection(0)
-                spinner.isEnabled = true
+
+            cine_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    cont += 1
+                    if (!topicsarray.contains(1)) topicsarray.add(1)
+
+                } else if (cont == 1) {
+                    cine_checkbox.isChecked = true
+
+                } else {
+                    cont -= 1
+                    topicsarray.remove(1)
+
+                }
+                validar()
+            }
+
+            ciencia_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    cont += 1
+                    if (!topicsarray.contains(3)) topicsarray.add(3)
+                } else if (cont == 1) {
+                    ciencia_checkbox.isChecked = true
+                } else {
+                    cont -= 1
+                    topicsarray.remove(3)
+                }
+                validar()
+            }
+            deporte_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    cont += 1
+                    if (!topicsarray.contains(4)) topicsarray.add(4)
+                } else if (cont == 1) {
+                    deporte_checkbox.isChecked = true
+                } else {
+                    cont -= 1
+                    topicsarray.remove(4)
+                }
+                validar()
+            }
+            arte_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    cont += 1
+                    if (!topicsarray.contains(5)) topicsarray.add(5)
+                } else if (cont == 1) {
+                    arte_checkbox.isChecked = true
+                } else {
+                    cont -= 1
+                    topicsarray.remove(5)
+                }
+                validar()
+            }
+            musica_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    cont += 1
+                    if (!topicsarray.contains(2)) topicsarray.add(2)
+                } else if (cont == 1) {
+                    musica_checkbox.isChecked = true
+                } else {
+                    cont -= 1
+                    topicsarray.remove(2)
+                }
+                validar()
+            }
+            videojuegos_checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    prueba.text = topicsarray[0].toString()
+                    cont += 1
+                    if (!topicsarray.contains(6)) topicsarray.add(6)
+                } else if (cont == 1) {
+                    videojuegos_checkbox.isChecked = true
+                } else {
+                    cont -= 1
+                    topicsarray.remove(6)
+                }
+                validar()
+            }
+
+
+            todoschek.setOnClickListener {
+                if (todoschek.isChecked == true) {
+                    todoschek.isEnabled = false
+                    ciencia_checkbox.isChecked = true
+                    cine_checkbox.isChecked = true
+                    deporte_checkbox.isChecked = true
+                    musica_checkbox.isChecked = true
+                    arte_checkbox.isChecked = true
+                    videojuegos_checkbox.isChecked = true
+                    spinner.setSelection(1)
+                }
+            }
+
+            switch.setOnCheckedChangeListener { _, _ ->
+                if (!switch.isChecked) {
+                    spinnerpistas.isEnabled = false
+                }
+                if (switch.isChecked) {
+                    spinnerpistas.isEnabled = true
+                }
             }
         }
-    }
 
-    fun spinadp() {
-        if (cont < 6) {
-            val num_pre = arrayOf(5, 6, 7, 8, 9, 10)
-            adapterQuestions = ArrayAdapter<Int>(this, R.layout.support_simple_spinner_dropdown_item, num_pre)
-            spinner.setAdapter(adapterQuestions)
+        fun validar() {
+            when (cont) {
+                1 -> {
+                    spinadp()
+                    todoschek.isEnabled = true
+                    todoschek.isChecked = false
+                    spinner.setSelection(0)
+                    spinner.isEnabled = false
+                }
+                in 2..5 -> {
+                    spinadp()
+                    todoschek.isEnabled = true
+                    todoschek.isChecked = false
+                    spinner.setSelection(0)
+                    spinner.isEnabled = true
+                }
+                6 -> {
+                    spinadp()
+                    todoschek.isChecked = true
+                    todoschek.isEnabled = false
+                    spinner.setSelection(0)
+                    spinner.isEnabled = true
+                }
+            }
         }
 
-        if (cont == 6) {
-            val preg = arrayListOf<Int>(6, 7, 8, 9, 10)
-            adapterQuestions = ArrayAdapter<Int>(this, R.layout.support_simple_spinner_dropdown_item, preg)
-            spinner.setAdapter(adapterQuestions)
+        fun spinadp() {
+            if (cont < 6) {
+                val num_pre = arrayOf(5, 6, 7, 8, 9, 10)
+                adapterQuestions = ArrayAdapter<Int>(this, R.layout.support_simple_spinner_dropdown_item, num_pre)
+                spinner.setAdapter(adapterQuestions)
+            }
+
+            if (cont == 6) {
+                val preg = arrayListOf<Int>(6, 7, 8, 9, 10)
+                adapterQuestions = ArrayAdapter<Int>(this, R.layout.support_simple_spinner_dropdown_item, preg)
+                spinner.setAdapter(adapterQuestions)
+            }
+
         }
 
-    }
+        fun selectdificulty() {
+            if (Alto.isChecked) dificultad = 3
+            if (medio.isChecked) dificultad = 2
+            if (bajo.isChecked) dificultad = 1
+        }
 
-    fun selectdificulty() {
-        if (Alto.isChecked) dificultad = 3
-        if (medio.isChecked) dificultad = 2
-        if (bajo.isChecked) dificultad = 1
-    }
-
-    override fun onBackPressed() {
-        flagenvio = true
-        selectdificulty()
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("topicsarray", topicsarray)
-        intent.putExtra("intNoQuestions", spinner.getSelectedItem().toString().toInt());
-        intent.putExtra("intNopistas", spinnerpistas.getSelectedItem().toString().toInt());
-        intent.putExtra("dificultad", dificultad)
-        intent.putExtra("intspinpistas", switch.isChecked)
+        override fun onBackPressed() {
+            flagenvio = true
+            selectdificulty()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("topicsarray", topicsarray)
+            intent.putExtra("intNoQuestions", spinner.getSelectedItem().toString().toInt());
+            intent.putExtra("intNopistas", spinnerpistas.getSelectedItem().toString().toInt());
+            intent.putExtra("dificultad", dificultad)
+            intent.putExtra("intspinpistas", switch.isChecked)
 //        intent.putExtra("flagop", flagenvio)
-        startActivity(intent)
+            startActivity(intent)
 
+
+        }
 
     }
 
-}
